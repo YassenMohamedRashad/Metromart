995bf387c2fda9713d5c6b685f9e7337392f9859
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2024-01-03 19:53:41 +0200

    Revert "Hyman"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Hyman</div>
+    <div>My team app</div>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
-import Nav from "./Components/Navbar";
-import Footer from "./Components/Footer";
-import Login from "./Components/Login";
-import Signup from "./Components/SignUp";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/nav.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Nav />
-
-			<Routes>
-				<Route path="/Metromart/" element={<App />} />
-				<Route path="/Metromart/login" element={<Login />} />
-				<Route path="/Metromart/signUp" element={<Signup />} />
-			</Routes>
-
-			<Footer />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
