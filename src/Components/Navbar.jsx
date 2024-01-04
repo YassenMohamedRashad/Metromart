2749a18fb3f6c41071efd02a9bbff0a9c5f3a0ea
AhmadOthman <trollingosman101@outlook.com>
2024-01-04 21:14:53 +0200

    added assets

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Navbar, Nav, Container, Form, FormControl } from "react-bootstrap";
+import SearchIcon from "../assets/images/icons/Search_icon.png";
+function nav() {
+	return (
+		<Navbar bg="light" expand="lg" className="mb-3">
+			<Container>
+				<Navbar.Brand href="#home">MetroMart</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="m-auto">
+						<Nav.Link href="#home" className="me-4">
+							Home
+						</Nav.Link>
+						<Nav.Link href="#contact" className="me-4">
+							Contact
+						</Nav.Link>
+						<Nav.Link href="#contact" className="me-4">
+							About
+						</Nav.Link>
+						<Nav.Link href="#signup" className="me-4">
+							Sign Up
+						</Nav.Link>
+					</Nav>
+					<Form className="d-flex">
+						<FormControl
+							type="search"
+							placeholder="What are you looking for ?"
+							aria-label="Search"
+						/>
+						<button type="submit">
+							<img src={SearchIcon} alt="Search" />
+						</button>
+					</Form>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default nav;
---
+++ src/Components/footer.jsx
@@ -0,0 +1,71 @@
+import send from "../assets/images/icons/send.svg";
+function Footer() {
+	return (
+		<>
+			<footer className="mt-3">
+				<div className="cont">
+					<div className="column">
+						<div className="d-flex flex-column gap-3">
+							<h4>MetroMart</h4>
+							<h6 className="fw-medium">Subscribe</h6>
+							<p>Get 10% off your first order</p>
+						</div>
+						<form method="get" className="d-flex">
+							<div className="col">
+								<div className="mb-3">
+									<input
+										type="email"
+										className="form-control"
+										name="FooterEmail"
+										id="FooterEmail"
+										placeholder="Enter Your Email"
+									/>
+									<button type="submit" className="btn send">
+										<img
+											src={send}
+											className="img-fluid"
+											alt="send"
+										/>
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+					<div className="column">
+						<h5 className="mb-4">Support</h5>
+						<div className="d-flex flex-column gap-1">
+							<p>Zakir Hussein st., Nasr City, Cairo, Egypt</p>
+							<p>
+								<a href="mailto:MetroMart@example.com">
+									MetroMart@example.com
+								</a>
+							</p>
+							<p>+20 123 456 7890</p>
+						</div>
+					</div>
+					<div className="column">
+						<h5 className="mb-4">account</h5>
+						<div className="d-flex flex-column justify-content-center">
+							<p>my account</p>
+							<p>login/register</p>
+							<p>cart</p>
+							<p>wishlist</p>
+							<p>shop</p>
+						</div>
+					</div>
+					<div className="column"></div>
+					<div className="column"></div>
+				</div>
+				<hr />
+				<div className="d-sm-flex justify-content-center py-1 copyright ">
+					<p className="text-center">
+						© Copyright <a className="fw-bold mx-1">MetroMart</a>
+						2023. All right reserved
+					</p>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
