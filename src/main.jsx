028f0d19cdb4b54637d207b20c84c44fcf13cfd6
AhmadOthman <trollingosman101@outlook.com>
2024-01-04 21:00:49 +0200

    init project

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-/* libraries and modules imports */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-/* components imports */
-import App from "./App";
-import Nav from "./Components/Navbar";
-import Footer from "./Components/footer.jsx";
-import Login from "./Components/Login";
-/* css files imports */
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/nav.css";
-import "./assets/css/footer.css";
-import "./assets/css/App.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Nav />
-
-			<Routes>
-				<Route path="/e-commerce/" element={<App />} />
-				<Route path="/e-commerce/login" element={<Login />} />
-			</Routes>
-
-			<Footer />
-		</BrowserRouter>
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
