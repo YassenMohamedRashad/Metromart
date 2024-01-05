b0cb12b99cfbfcf752500af235e84e86bdda23bd
AhmadOthman <trollingosman101@outlook.com>
2024-01-05 21:02:17 +0200

    rename index to main

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,13 @@ import App from "./App";
 import Nav from "./Components/Navbar";
 import Footer from "./Components/footer.jsx";
 import Login from "./Components/Login";
+import ProductDetails from "./Components/productDetails.jsx";
 /* css files imports */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/nav.css";
 import "./assets/css/footer.css";
 import "./assets/css/ProductDetails.css";
 import "./assets/css/App.css";
-import ProductDetails from "./Components/productDetails.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
