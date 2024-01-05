9f6ea0647fb53dc504e18956cad96e1e016ded67
YassenMohamed <yassenmohamedrashad@gmail.com>
2024-01-05 13:50:04 +0200

    fix router dom

--- src/index.jsx
+++ src/index.jsx
@@ -10,17 +10,18 @@ import AccountDetails from "./Components/AccountDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/nav.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot( document.getElementById( "root" ) );
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Nav />
 
+
 			<Routes>
-				<Route path="/Metromart/" element={<App />} />
-				<Route path="/Metromart/login" element={<Login />} />
-				<Route path="/Metromart/signup" element={<Signup />} />
-				<Route path="/Metromart/accountdetails" element={<AccountDetails />} />
+				<Route path="/Metromart/" element={ <App /> } />
+				<Route path="/Metromart/login" element={ <Login /> } />
+				<Route path="/Metromart/signup" element={ <Signup /> } />
+				<Route path="/Metromart/accountdetails" element={ <AccountDetails /> } />
 			</Routes>
 
 			<Footer />
