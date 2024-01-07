3fd9f7fe52e9708d5cd1abeec70a2b526ce31667
YassenMohamed <yassenmohamedrashad@gmail.com>
2024-01-07 21:04:43 +0200

    .

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -11,32 +11,6 @@ function Login() {
 	}
 	
 	return (
-<<<<<<< HEAD
-		<div className="container">
-			<div className="row signup p-3">
-				<div className="col-md-6 d-md-block d-none">
-					<img src={signupImage} className="w-100" alt=''/>
-				</div>
-
-				<div className="col-md-6">
-					<h2 className="fw-bolder">Create an account</h2>
-					<form action="post" className="form signup-form" >
-						<h6>Enter your details below</h6>
-                        <input type="text" name="name" className="mt-5 name-input w-100" required/>
-						<label className="name-label">Name</label>
-						<br />
-						<input type="text" name="email" className="mt-5 email-input w-100" required/>
-						<label className="email-label">Email or Phone Number</label>
-						<br />
-						<input type="password" name="password" className="mt-5 pass-input w-100" required/>
-						<label className="pass-label">Password</label>
-						<br />
-						<button className="btn btn-danger mt-4 w-100" type="submit" style={{height:50}}>Create Account</button>
-						<br />
-					</form>
-					<button className="btn btn-light border mt-2 w-100" style={{height:50}}><img src={googleLogo} /> Sign Up with Google</button>
-					<h6 className="text-secondary mt-4">Aleardy have account? <a href="/Metromart/login" className="text-secondary" > Log in</a></h6>
-=======
 		<div className="container mb-5">
 			<div className="row signup align-items-center justify-content-center d-flex">
 				<div className="col-lg-6 d-lg-block d-none ">
@@ -108,7 +82,6 @@ function Login() {
 						<button className="btn btn-light border mt-2 w-100 " style={{ height: 50 }}><img src={googleLogo} /> Sign Up with Google</button>
 						<h6 className="text-secondary mt-4">Aleardy have account? <a href="/Metromart/login" className="text-secondary" > Log in</a></h6>
 					</div>
->>>>>>> Hyman
 				</div>
 			</div>
 		</div>
