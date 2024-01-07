604bba791f718b9c50a098f1a78a6c464947f06f
Zeyad Hyman <zeyadhyman20@gmail.com>
2024-01-07 20:25:51 +0200

    Added needed inputs to signup page

--- src/Components/AccountDetails.jsx
+++ src/Components/AccountDetails.jsx
@@ -44,36 +44,36 @@ function AccountDetails() {
                     </div>
 
                     <div className='col-8 manage-my-account mt-5 '>
-                        <form method='post' class="shadow-sm rounded p-5">
+                        <form method='post' class="shadow rounded p-5">
                             <h4 className=' text-danger '>Edit Your Profile</h4>
                             <div className="d-flex justify-content-between mt-3">
                                 <div>
                                     <label htmlFor="FirstName" className='fw-bold'>First Name</label>
-                                    <input type="text" name="firstname" className='form-control bg-light text-secondary border-0 mt-2  account-inputs ' placeholder='First Name' onChange={changeFirstname} />
+                                    <input type="text" name="firstname" className='form-control text-black shadow-sm bg-light text-secondary border-0 mt-2  account-inputs ' placeholder='First Name' onChange={changeFirstname} />
                                 </div>  
                                 <div>
                                     <label htmlFor="LastName" className='fw-bold'>Last Name</label>
-                                    <input type="text" name="lastname" className='form-control bg-light text-secondary border-0 mt-2 account-inputs' placeholder='Second Name' onChange={changeLastname} />
+                                    <input type="text" name="lastname" className='form-control text-black shadow-sm bg-light text-secondary border-0 mt-2 account-inputs' placeholder='Second Name' onChange={changeLastname} />
                                 </div>
                             </div>
                             <div className="d-flex justify-content-between mt-3">
                                 <div>
                                     <label htmlFor="Email" className='fw-bold'>Email</label>
-                                    <input type="email" placeholder='Email' name="email" className='form-control bg-light text-secondary border-0 mt-2 account-inputs'/>
+                                    <input type="email" placeholder='Email' name="email" className='form-control shadow-sm text-black bg-light text-secondary border-0 mt-2 account-inputs'/>
                                 </div>
                                 <div>
                                     <label htmlFor="Adress" className='fw-bold'>Address</label>
-                                    <input type="text" placeholder='Address' name="address" className='form-control bg-light text-secondary border-0 mt-2 account-inputs'/>
+                                    <input type="text" placeholder='Address' name="address" className='form-control shadow-sm text-black bg-light text-secondary border-0 mt-2 account-inputs'/>
                                 </div>
                             </div>
 
                             <label htmlFor="PasswordChanges" className='fw-bold mt-4'>Password Changes</label>
-                            <input type="password" name="currentPassword" placeholder='Current Password' className='form-control bg-light text-secondary border-0 mt-2' style={{ height: 50 }} />
-                            <input type="password" name="newPassword" placeholder='New Password' className='form-control bg-light text-secondary border-0 mt-2' style={{ height: 50 }} />
-                            <input type="password" name="confirmNewPassword" placeholder='Confirm New Password' className='form-control bg-light text-secondary border-0 mt-2' style={{ height: 50 }} />
+                            <input type="password" name="currentPassword" placeholder='Current Password' className='form-control text-black shadow-sm bg-light text-secondary border-0 mt-2' style={{ height: 50 }} />
+                            <input type="password" name="newPassword" placeholder='New Password' className='form-control text-black shadow-sm bg-light text-secondary border-0 mt-2' style={{ height: 50 }} />
+                            <input type="password" name="confirmNewPassword" placeholder='Confirm New Password' className='form-control text-black shadow-sm bg-light text-secondary border-0 mt-2' style={{ height: 50 }} />
 
                             <div className="d-flex justify-content-end mt-5">
-                                <button className='btn btn-light text-black me-3'>Cancel</button>
+                                <button className='btn shadow-sm btn-light text-black me-3'>Cancel</button>
                                 <button className='btn btn-danger p-3' type='submit'>Save Changes</button>
                             </div>
                         </form>
