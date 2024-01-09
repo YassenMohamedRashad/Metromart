1be5eb1d1c91312faec27090ab3214ee2ea5802d
YassenMohamed <yassenmohamedrashad@gmail.com>
2024-01-09 19:36:31 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import AccountDetails from './Components/AccountDetails';
-import Wishlist from "./Components/Wishlist"
-import axios from 'axios';
 
 function App ()
 {
--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 // importing bootstrap files
 import"../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import"../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import"bootstrap/dist/js/bootstrap.bundle.min.js";
 // importing stylesheet
 import "../assets/css/Wishlist.css"
 // importing navbar and footer
