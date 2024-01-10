807534f4b79af9d24576a26ad85e4e5be9b2c529
YassenMohamed <yassenmohamedrashad@gmail.com>
2024-01-10 20:21:37 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 function App ()
 {
+  const [ data, setData ] = useState( [] );
+  const getData = async() =>
+  {
+    const res = await axios.get( "http://localhost:5011" )
+    console.log( res.data );
+  }
+  
+  useEffect( () =>
+  {
+    getData()
+  },[])
+
+
   return (
     <>
       app
