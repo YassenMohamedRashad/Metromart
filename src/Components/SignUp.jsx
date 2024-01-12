8402bb6027a2b55743393b697f2609ffa6aa6a44
AhmadOthman <trollingosman101@outlook.com>
2024-01-12 14:16:10 +0200

    signup page done

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -3,139 +3,305 @@ import signupImage from "../assets/images/signup.svg";
 import googleLogo from "../assets/images/google-icon.svg";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-function SignUp() {	
-	
-	const get = async()=>{
-		const res = await axios.get("https://tryingtodeploy-rton.onrender.com/")
-		console.log(res.data)
-	
-	}
+// const link = "https://tryingtodeploy-rton.onrender.com/";
+function SignUp() {
+	/* Requests to the backend */
+	const get = async () => {
+		const res = await axios.get("http://localhost:5011");
+		console.log(res.data);
+	};
+	/* updating the state of the component */
 	useEffect(() => {
-		get()
-	},[])
+		get();
+	}, []);
+	/* setting the states of the app */
+	const [gender, setGender] = useState("");
+	const [address1, setAddress1] = useState("");
+	const [address2, setAddress2] = useState("");
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [age, setAge] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
 
-	const [address2, setAddress2] = useState('')	
+	/* handling changes */
 	const handleChange = (e) => {
-		setAddress2(e.target.value);
-	}
+		/**
+		 * Updates the state variables based on the user's input in the form fields.
+		 * @param {Object} e - The event object triggered by the user's input.
+		 */
+		const { name, value } = e.target;
+		const updateStateVariable = (setter) => setter(value);
+		const stateVariableMap = {
+			gender: setGender,
+			age: setAge,
+			address1: setAddress1,
+			address2: setAddress2,
+			username: setUsername,
+			email: setEmail,
+			password: setPassword,
+			confirmPassword: setConfirmPassword,
+			phoneNumber: setPhoneNumber,
+		};
+		const setter = stateVariableMap[name];
+		if (setter) {
+			updateStateVariable(setter);
+			console.log(name, value);
+		}
+	};
 
-	// Validatoin
-	const [error_pass, setError_pass] = useState("")
-	const [error_email, setError_email] = useState("")
-	const [ error_phonenumber, setError_phonenumber ] = useState( "" )
-	
-	const handleSubmit = (e) => {
-		
+	// Validation
+	const [error_pass, setError_pass] = useState(false);
+	const [error_email, setError_email] = useState(false);
+	const [error_phonenumber, setError_phonenumber] = useState(false);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
 		// password validation
-		const password = e.target.password.value;
-		const confirmPassword = e.target.confirmpassword.value;
-		if (password !== confirmPassword) {
-			e.preventDefault();
-			setError_pass( "Password not matched" )
-		} else{
-			setError_pass("")
+		if (password != confirmPassword) {
+			setError_pass("Passwords don't match");
+		} else {
+			setError_pass(false);
 		}
 
 		// email validation
-		const email = e.target.email.value;
 		const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-		if ( !emailRegex.test( email ) )
-		{
-			e.preventDefault();
-			setError_email("Enter Valid Email")
+		if (!emailRegex.test(email)) {
+			setError_email("Enter a Valid Email");
 		} else {
-			setError_email("")
+			setError_email(false);
 		}
 
 		// phonenumber validation
-		const phoneNumber = e.target.phonenumber.value;
-		const phoneNumberRegex = /^0\d{10}$/;
+		const phoneNumberRegex = /^0\d{10}$/g;
 		if (!phoneNumberRegex.test(phoneNumber)) {
-			setError_phonenumber("Enter Valid Phone Number")
+			setError_phonenumber("Enter a Valid Phone Number");
 		} else {
-			setError_phonenumber("")
+			setError_phonenumber(false);
 		}
-		
+		if (!error_pass && !error_email && !error_phonenumber) {
+			const userData = {
+				name: username,
+				email: email,
+				password: password,
+				address1: address1,
+				address2: address1,
+				phone_number: phoneNumber,
+				gender: gender,
+				age: age,
+			};
+			console.log(userData);
+			// Send a POST request to your API endpoint
+			const res = await axios.post(
+				"http://localhost:5011/user/signup",
+				userData
+			);
+			console.log(res.data);
+			if (res.data === "ERROR: this email is exists") {
+				throw new Error("duplicated Entry");
+			}
+		}
 	};
 
 	return (
 		<div className="container mb-5">
 			<div className="row signup align-items-center justify-content-center d-flex">
 				<div className="col d-xl-block d-none ">
-					<img src={signupImage} className="w-100 signup-image" alt='' />
+					<img
+						src={signupImage}
+						className="w-100 signup-image"
+						alt=""
+					/>
 				</div>
 
 				<div className="col justify-content-center d-flex">
 					<div className="mt-5 mb-5">
-						<h1 className="fw-bolder">	Create an Account</h1>
-						<form className="form signup-form " onSubmit={handleSubmit}>
+						<h1 className="fw-bolder"> Create an Account</h1>
+						<form
+							className="form signup-form"
+							method="post"
+							onSubmit={handleSubmit}
+						>
 							<h5>Enter your Details Below</h5>
 							{/* Name input */}
 
-							<input type="text" name="name" className="mt-5 name-input inputs" required />
+							<input
+								type="text"
+								name="username"
+								className="mt-5 name-input inputs"
+								required
+								onChange={handleChange}
+							/>
 							<label className="name-label">Name</label>
 							<br />
 
 							{/* Email input */}
-							<input type="text" name="email" className="mt-5 email-input inputs" required />
+							<input
+								type="text"
+								name="email"
+								className="mt-5 email-input inputs"
+								required
+								onChange={handleChange}
+							/>
 							<label className="email-label">Email</label>
 							<br />
-							<span className="text-danger mt-2 " style={{ marginLeft: 366 }}>{error_email}</span>
+							<span
+								className="text-danger mt-2 "
+								style={{ marginLeft: 366 }}
+							>
+								{error_email}
+							</span>
 							<br />
 
 							{/* Password input */}
-							<input type="password" name="password" className="mt-4 pass-input inputs" required />
+							<input
+								type="password"
+								name="password"
+								className="mt-4 pass-input inputs"
+								required
+								onChange={handleChange}
+							/>
 							<label className="pass-label">Password</label>
 							<br />
 
 							{/* Confirm Password input */}
-							<input type="password" name="confirmpassword" className="mt-5 confirmpass-input inputs" required />
-							<label className="confirmpass-label">Confirm Password</label>
+							<input
+								type="password"
+								name="confirmPassword"
+								className="mt-5 confirmpass-input inputs"
+								required
+								onChange={handleChange}
+							/>
+							<label className="confirmpass-label">
+								Confirm Password
+							</label>
 							<br />
-							<span className="text-danger mt-2" style={{ marginLeft: 320 }}>{error_pass}</span>
+							<span
+								className="text-danger mt-2"
+								style={{ marginLeft: 320 }}
+							>
+								{error_pass}
+							</span>
 							<br />
 
 							{/* Address one input */}
-							<input type="text" name="address1" className="mt-4 address1-input inputs" required />
-							<label className="address1-label">Address One</label>
+							<input
+								type="text"
+								name="address1"
+								className="mt-4 address1-input inputs"
+								required
+								onChange={handleChange}
+							/>
+							<label className="address1-label">
+								Address One
+							</label>
 							<br />
 
 							{/* Address two input */}
-							<input ype="text" name="address2" className="mt-4 address2-input inputs" onChange={handleChange} />
-							<label className={`address2-label ${address2 ? 'active' : ''}`}>Address Two</label>
+							<input
+								ype="text"
+								name="address2"
+								className="mt-4 address2-input inputs"
+								onChange={handleChange}
+							/>
+							<label
+								className={`address2-label ${
+									address2 ? "active" : ""
+								}`}
+							>
+								Address Two
+							</label>
 							<br />
 
 							{/* Phone number input */}
-							<input type="tel" name="phonenumber" className="mt-5 phonenumber-input inputs" required />
-							<label className="phonenumber-label">Phone Number</label>
+							<input
+								type="tel"
+								name="phoneNumber"
+								className="mt-5 phonenumber-input inputs"
+								required
+								onChange={handleChange}
+							/>
+							<label className="phonenumber-label">
+								Phone Number
+							</label>
 							<br />
-							<span className="text-danger mt-2" style={{ marginLeft: 295 }}>{error_phonenumber}</span>
+							<span
+								className="text-danger mt-2"
+								style={{ marginLeft: 295 }}
+							>
+								{error_phonenumber}
+							</span>
 							<br />
 
 							{/* Age input */}
-							<input type="number" name="age" className="mt-4 age-input inputs" required />
+							<input
+								type="number"
+								name="age"
+								className="mt-4 age-input inputs"
+								required
+								onChange={handleChange}
+							/>
 							<label className="age-label">Age</label>
 							<br />
 
 							{/* Gender input */}
-							<label className="mt-4 text-secondary">Gender</label>
+							<label className="mt-4 text-secondary">
+								Gender
+							</label>
 							<div className="d-flex">
-								<div class="form-check mt-2 me-4">
-									<input class="form-check-input male-input shadow" type="radio" name="gender" value={'male'} />
-									<label class="form-check-label male-label">Male</label>
+								<div className="form-check mt-2 me-4">
+									<input
+										className="form-check-input male-input shadow"
+										type="radio"
+										name="gender"
+										value={"male"}
+										onChange={handleChange}
+									/>
+									<label className="form-check-label male-label">
+										Male
+									</label>
 								</div>
-								<div class="form-check mt-2 ">
-									<input class="form-check-input female-input shadow" type="radio" name="gender" value={'female'} />
-									<label class="form-check-label female-label">Female</label>
+								<div className="form-check mt-2 ">
+									<input
+										className="form-check-input female-input shadow"
+										type="radio"
+										name="gender"
+										value={"female"}
+										onChange={handleChange}
+									/>
+									<label className="form-check-label female-label">
+										Female
+									</label>
 								</div>
 							</div>
 
-							<button className="btn btn-danger mt-4 w-100" type="submit" style={{height: 50 }}>Create Account</button>
+							<button
+								className="btn btn-danger mt-4 w-100"
+								type="submit"
+								style={{ height: 50 }}
+							>
+								Create Account
+							</button>
 							<br />
 						</form>
-						<button className="btn btn-light border mt-2 w-100 " style={{ height: 50 }}><img src={googleLogo} /> Sign Up with Google</button>
-						<h6 className="text-secondary mt-4">Aleardy have account? <a href="/Metromart/login" className="text-secondary" > Log in</a></h6>
+						<button
+							className="btn btn-light border mt-2 w-100 "
+							style={{ height: 50 }}
+						>
+							<img src={googleLogo} /> Sign Up with Google
+						</button>
+						<h6 className="text-secondary mt-4">
+							Already have account?{" "}
+							<a
+								href="/Metromart/login"
+								className="text-secondary"
+							>
+								{" "}
+								Log in
+							</a>
+						</h6>
 					</div>
 				</div>
 			</div>
