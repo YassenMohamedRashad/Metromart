a1389d04b09c7e4ee70705402189f7a203a005bc
AhmadOthman <trollingosman101@outlook.com>
2024-01-13 13:45:11 +0200

    Update the roadmap component UI and CSS. done
     Changed the selector for the hover effect from `a` to `.tab`.

--- src/Components/Roadmap.jsx
+++ src/Components/Roadmap.jsx
@@ -4,14 +4,21 @@ import pipeLine from "../assets/images/Line 13.svg";
 export const Roadmap = () => {
 	return (
 		<>
-			<div className="tabs px-2">
+			{/* <div className="tabs px-2">
 				<p className="tab">text1</p>
-				{/* <img src={pipeLine} className="img-fluid rounded-top" alt="" /> */}
+				<img src={pipeLine} className="img-fluid rounded-top" alt="" />
 				<span className="pipeline"></span>
 
 				<p className="tab">text2</p>
 				<p className="tab">text3</p>
-			</div>
+			</div> */}
+			<h6 className="tabs">
+				<a className="tab">Account</a> /{" "}
+				<a className="tab">Gaming</a> /{" "}
+				<a className="tab">Product Name</a>{" "}
+			</h6>
+			<br />
+			<br />
 		</>
 	);
 };
