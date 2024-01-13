04e6d26586ab97b48c97274e22ace47c2363467c
AhmadOthman <trollingosman101@outlook.com>
2024-01-13 13:51:24 +0200

    Remove the pipeline image and the text tabs.

--- src/Components/Roadmap.jsx
+++ src/Components/Roadmap.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import "../assets/css/Roadmap.css";
-import pipeLine from "../assets/images/Line 13.svg";
 export const Roadmap = () => {
 	return (
 		<>
-			{/* <div className="tabs px-2">
-				<p className="tab">text1</p>
-				<img src={pipeLine} className="img-fluid rounded-top" alt="" />
-				<span className="pipeline"></span>
-
-				<p className="tab">text2</p>
-				<p className="tab">text3</p>
-			</div> */}
-			<h6 className="tabs">
+			<h6 className="tabs mb-5">
 				<a className="tab">Account</a> /{" "}
 				<a className="tab">Gaming</a> /{" "}
 				<a className="tab">Product Name</a>{" "}
 			</h6>
-			<br />
-			<br />
 		</>
 	);
 };
