97f3bc03185c6b94de385170c5403ba112a6e1a5
AhmadOthman <trollingosman101@outlook.com>
2024-01-13 19:13:54 +0200

    Update ProductDetails and Slider components to use dummy data instead of making requests to a database.

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,8 +1,35 @@
+/* dependencies */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 /* assets */
 import { Roadmap } from "./Roadmap";
 import { Slider } from "./Slider";
 import "../assets/css/ProductDetails.css";
+
 const ProductDetails = () => {
+	const [data, setData] = useState(null);
+	/* Request to the Actual Database
+		axios
+			.get("http://localhost:5011/products/5")
+			.then((res) => {
+				setData([res.data.data]);
+				console.log([res.data.data]); // Store the data in the state variable
+			})
+			.catch((error) => {
+				console.log(error);
+			}); */
+	/* Request for testing */
+	useEffect(() => {
+		axios
+			.get("https://dummyjson.com/products/8")
+			.then((res) => {
+				setData(res.data);
+			})
+			.catch((error) => {
+				alert(error);
+			});
+	}, []);
+
 	return (
 		<>
 			<div className="container-fluid">
@@ -10,7 +37,8 @@ const ProductDetails = () => {
 				<div className="row d-flex justify-content-between">
 					<div className="col">
 						<div className="slider-Container mb-5">
-							<Slider />
+							{data && <Slider images={data.images} />}
+							{/* Check if data is not null before rendering Slider */}
 						</div>
 					</div>
 					<div className="col">
@@ -21,4 +49,5 @@ const ProductDetails = () => {
 		</>
 	);
 };
+
 export default ProductDetails;
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,9 +1,7 @@
+import { useRef } from "react";
 import "../assets/css/Slider.css";
-import React, { useEffect, useState, useRef } from "react";
-import axios from "axios";
 
-export const Slider = () => {
-	const [data, setData] = useState(null); // Create a state variable to store the data
+export const Slider = ({ images }) => {
 	const bigImageRef = useRef(); // Create a ref for the big image
 
 	const handleClick = (e) => {
@@ -12,40 +10,18 @@ export const Slider = () => {
 		bigImageRef.current.src = smallImageSrc; // Set the src of the big image to the stored src of the small image
 	};
 
-	useEffect(() => {
-		/* Request to the Actual Database
-		axios
-			.get("http://localhost:5011/products/5")
-			.then((res) => {
-				setData([res.data.data]);
-				console.log([res.data.data]); // Store the data in the state variable
-			})
-			.catch((error) => {
-				console.log(error);
-			}); */
-		/* Request for testing */
-		axios
-			.get("https://dummyjson.com/products/8")
-			.then((res) => {
-				setData(res.data); // Store the data in the state variable
-			})
-			.catch((error) => {
-				alert(error);
-			});
-	}, []);
-
 	return (
 		<div className="product">
 			<div className="product-small-img">
-				{data &&
-					data.images
-						.filter((path) => path !== data.images[0])
+				{images &&
+					images
+						.filter((path) => path !== images[0])
 						.map((path, index) => (
 							<img key={index} src={path} onClick={handleClick} />
 						))}
 			</div>
 			<div className="big-img-container">
-				<img ref={bigImageRef} src={data && data.images[0]} />
+				<img ref={bigImageRef} src={images && images[0]} />
 			</div>
 		</div>
 	);
