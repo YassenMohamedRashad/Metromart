918e7b9c335380556841f47e1df5e60416fd9154
AhmadOthman <trollingosman101@outlook.com>
2024-01-13 14:03:19 +0200

    Remove the product details card.

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -5,18 +5,8 @@ const ProductDetails = () => {
 	return (
 		<>
 			<Roadmap />
-			<div className="card">
 				<img src={img1} className="card-img-top" alt="" />
-				<div className="card-body">
-					<h5 className="card-title">Havic HV-G92 Gamepad</h5>
-					<p className="card-text">
-						$192.00 <span className="discount">$120.00</span>
-					</p>
-					<a href="#" className="btn btn-primary">
-						Buy Now
-					</a>
-				</div>
-			</div>
+			
 		</>
 	);
 };
