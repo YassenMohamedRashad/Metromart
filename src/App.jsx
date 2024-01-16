4e43489da34db1fef16a1cf953450754d5c6ac3e
AhmedOsman101 <trollingosman101@outlook.com>
2024-01-16 11:53:58 +0200

    Added About Us and Shop pages, a 404 page, and updated the navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -12,45 +12,51 @@ import BillingDetails from "./Components/BillingDetails";
 import ProductDetails from "./Components/ProductDetails";
 import { Home } from "./Components/Home";
 
-function App() {
-	const [userData, setUserData] = useState(null);
-	const [token, setToken] = useState(null);
+import Error from "./Components/Error";
+import AboutUs from "./Components/Aboutus";
+import Shop from "./Components/Shop";
 
-	const handleUserData = (data) => setUserData(data);
-	const handleToken = (data) => setToken(data);
-	return (
-		<>
-			<Nav />
-			<Routes>
-				<Route path="/Metromart/" element={<Home />} />
-				<Route
-					path="/Metromart/login"
-					element={
-						<Login
-							handleUserData={handleUserData}
-							handleToken={handleToken}
-						/>
-					}
-				/>
-				<Route path="/Metromart/signup" element={<Signup />} />
-				<Route
-					path="/Metromart/accountdetails"
-					element={<AccountDetails />}
-				/>
-				<Route path="/Metromart/wishlist" element={<WishList />} />
-				<Route
-					path="/Metromart/billingdetails"
-					element={<BillingDetails />}
-				/>
-				<Route
-					path="/Metromart/productDetails"
-					element={<ProductDetails />}
-				/>
-				<Route path="/Metromart/*" element={<h1>Error</h1>}></Route>
-			</Routes>
-			<Footer />
-		</>
-	);
+function App() {
+    const [userData, setUserData] = useState(null);
+    const [token, setToken] = useState(null);
+
+    const handleUserData = (data) => setUserData(data);
+    const handleToken = (data) => setToken(data);
+    return (
+        <>
+            <Nav />
+            <Routes>
+                <Route path="/Metromart/aboutus" element={<AboutUs />} />
+                <Route path="/Metromart/shop" element={<Shop />} />
+                <Route path="/Metromart/" element={<Home />} />
+                <Route
+                    path="/Metromart/login"
+                    element={
+                        <Login
+                            handleUserData={handleUserData}
+                            handleToken={handleToken}
+                        />
+                    }
+                />
+                <Route path="/Metromart/signup" element={<Signup />} />
+                <Route
+                    path="/Metromart/accountdetails"
+                    element={<AccountDetails />}
+                />
+                <Route path="/Metromart/wishlist" element={<WishList />} />
+                <Route
+                    path="/Metromart/billingdetails"
+                    element={<BillingDetails />}
+                />
+                <Route
+                    path="/Metromart/productDetails"
+                    element={<ProductDetails />}
+                />
+                <Route path="/Metromart/*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
