6e938302e7747d543640c40c1d332fa67609b2fb
AhmadOthman <trollingosman101@outlook.com>
2024-01-19 01:16:08 +0200

    -  fix(Wishlist.jsx): remove unnecessary import of bootstrap bundle -  feat(Wishlist.jsx): fetch products from API and update wishlist products accordingly -  feat(Wishlist.jsx): add support for displaying product images and updating product prices with discounts

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -1,7 +1,5 @@
-// Importing React, useEffect, and useState
-import React, { useEffect, useState } from "react";
-// Importing Bootstrap files
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// Importing useEffect, and useState
+import { useEffect, useState } from "react";
 // Importing the stylesheet
 import "../assets/css/Wishlist.css";
 // Importing react-multi-carousel for the slider
@@ -9,7 +7,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 // Importing axios
 import axios from "axios";
-import products from "./Products.json";
+// import products from "./Products.json";
 import { useAuth } from "../Hooks/useAuth.jsx";
 
 // Responsiveness settings for the slider
@@ -39,23 +37,43 @@ const responsive = {
 // Wishlist component
 function WishList() {
 	const { wishlist } = useAuth();
-	// State to store the Products from the API
-	const [Products, setProducts] = useState([]);
+	const [Products, setProducts] = useState(null);
+
+	async function getProducts() {
+		try {
+			const res = await axios.get("http://localhost:5011/products/");
+			console.log(res.data.data);
+			return res.data.data;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	useEffect(() => {
-		let wishlistProducts = products.filter((item) => {
-			if (wishlist.includes(item.id)) return item;
-		});
-		setProducts([...wishlistProducts]);
+		const fetchData = async () => {
+			try {
+				const products = await getProducts();
+				let wishlistProducts = products.filter((item) => {
+					if (wishlist.includes(item.id)) {
+						item.images = JSON.parse(item.image_path);
+						return item;
+					}
+				});
+				setProducts(wishlistProducts);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
 	}, [wishlist]);
 
-	// Render the component
 	return (
 		<>
 			{/* Wishlist and For You list content */}
 			<div className="container my-container">
 				<div className="row wishlist-row">
 					<h4 className="col-xxl-10 col-xl-10 col-lg-9 col-md-8 col my-h4 ">
-						Wishlist ({Products && Products.length})
+						Wishlist ({Products && Products?.length})
 					</h4>
 					<button className="col btn btn-outline-dark btn-lg">
 						Move All To Cart
@@ -77,7 +95,7 @@ function WishList() {
 										{/* Sale button */}
 										<div>
 											<button className="sale btn btn-danger align-self-end">
-												-{item.discountPercentage}%
+												-{item.discount}%
 											</button>
 										</div>
 										{/* Trash icon */}
@@ -107,7 +125,7 @@ function WishList() {
 									{/* Add to cart button */}
 									<div className="card-body my-card-body">
 										<a
-											href="javascript:void(0)"
+											href="/Metromart/wishlist/"
 											className="btn btn-dark text-light w-100"
 										>
 											<svg
@@ -127,14 +145,20 @@ function WishList() {
 									{/* Price and sale */}
 									<p className="my-p">
 										<b>
-											{item.title}
+											{item.name}
 											<br />
 											<span className="text-danger">
 												${item.price} &nbsp;
 												<span className="text-secondary">
 													{" "}
 													<del>
-														${(item.price - item.price * (item.discountPercentage / 100)).toFixed(2)}
+														$
+														{(
+															item.price -
+															item.price *
+																(item.discount /
+																	100)
+														).toFixed(2)}
 													</del>
 												</span>
 											</span>
@@ -158,85 +182,92 @@ function WishList() {
 
 				<div className="row">
 					{/* Slider */}
-					<Carousel responsive={responsive}>
-						{/* Dynamic cards from API */}
-						{Products &&
-							Products.map((item) => (
-								<div className="col-10" key={item.id}>
-									{/* Card */}
-									<div className="card my-card">
-										<div>
-											{/* Sale button */}
+					{Products && Products.length > 0 && (
+						<Carousel responsive={responsive}>
+							{/* Dynamic cards from API */}
+							{Products &&
+								Products.map((item) => (
+									<div className="col-10" key={item.id}>
+										{/* Card */}
+										<div className="card my-card">
 											<div>
-												<button className="sale btn btn-danger align-self-end">
-													-{item.discountPercentage}%
-												</button>
-											</div>
-											{/* Eye icon */}
-											<a href="" className="eye-icon">
+												{/* Sale button */}
 												<div>
+													<button className="sale btn btn-danger align-self-end">
+														-{item.discount}%
+													</button>
+												</div>
+												{/* Eye icon */}
+												<a href="" className="eye-icon">
+													<div>
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															width="20"
+															height="20"
+															fill="currentColor"
+															className="bi bi-eye"
+															viewBox="0 0 16 16"
+														>
+															<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+															<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+														</svg>
+													</div>
+												</a>
+											</div>
+
+											{/* Image */}
+											<img
+												className="card-img-top image"
+												src={item.images[1]}
+												alt="Card image cap"
+											/>
+
+											{/* Add to cart button */}
+											<div className="card-body my-card-body">
+												<a
+													href="/Metromart/wishlist/"
+													className="btn btn-dark text-light w-100"
+												>
 													<svg
+														className="cart bi bi-cart"
 														xmlns="http://www.w3.org/2000/svg"
 														width="20"
 														height="20"
 														fill="currentColor"
-														className="bi bi-eye"
 														viewBox="0 0 16 16"
 													>
-														<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-														<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+														<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
 													</svg>
-												</div>
-											</a>
-										</div>
+													Add To Cart
+												</a>
+											</div>
 
-										{/* Image */}
-										<img
-											className="card-img-top image"
-											src={item.images[1]}
-											alt="Card image cap"
-										/>
-
-										{/* Add to cart button */}
-										<div className="card-body my-card-body">
-											<a
-												href="javascript:void(0)"
-												className="btn btn-dark text-light w-100"
-											>
-												<svg
-													className="cart bi bi-cart"
-													xmlns="http://www.w3.org/2000/svg"
-													width="20"
-													height="20"
-													fill="currentColor"
-													viewBox="0 0 16 16"
-												>
-													<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-												</svg>
-												Add To Cart
-											</a>
-										</div>
-
-										{/* Price and sale */}
-										<p className="my-p">
-											<b>
-												{item.title}
-												<br />
-												<span className="text-danger">
-													${item.price} &nbsp;
-													<span className="text-secondary">
-														{" "}
-														<del>
-															${(item.price - item.price * (item.discountPercentage / 100)).toFixed(2)}
-														</del>
+											{/* Price and sale */}
+											<p className="my-p">
+												<b>
+													{item.name}
+													<br />
+													<span className="text-danger">
+														${item.price} &nbsp;
+														<span className="text-secondary">
+															<del>
+																$
+																{(
+																	item.price -
+																	item.price *
+																		(item.discount /
+																			100)
+																).toFixed(2)}
+															</del>
+														</span>
 													</span>
-												</span>
-											</b>
-										</p>
+												</b>
+											</p>
+										</div>
 									</div>
-								</div>
-							))}
-					</Carousel>
+								))}
+						</Carousel>
+					)}
 				</div>
 			</div>
 		</>
