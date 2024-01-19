b421b1b79cca5a0e6113f33af9dd6dae76ac123a
YassenMohamed <yassenmohamedrashad@gmail.com>
2024-01-19 21:03:48 +0200

    .

--- src/Components/utils/Card.jsx
+++ src/Components/utils/Card.jsx
@@ -3,7 +3,7 @@ import unchecked_star_icon from "../../assets/images/unchecked-star-icon.png";
 import checked_star_icon from "../../assets/images/checked-star-icon.png";
 import axios from 'axios';
 import { useAuth } from '../../Hooks/useAuth';
-import {Success,Fail} from "../SweetAlert"
+import { Success, Fail, InfoAC } from "../SweetAlert"
 import { useNavigate } from 'react-router';
 
 
@@ -11,6 +11,30 @@ import { useNavigate } from 'react-router';
 function Card ({item})
 {
     const { user,user_token } = useAuth()
+
+    const handleWishlist = () =>
+    {
+        const isWishlist = localStorage.getItem('wishlist');
+
+        if (isWishlist) {
+            let userWishlist = JSON.parse(localStorage.getItem( 'wishlist' ))
+            if (item.id in userWishlist) {
+                InfoAC( "<h2>product already in wishlist</h2>" )
+            } else
+            {
+                userWishlist.push( item.id )
+                localStorage.setItem( 'wishlist', JSON.stringify( userWishlist ) )
+                Success("<h2>Product Added successfully</h2>")
+            }
+        } else
+        {
+            localStorage.setItem( 'wishlist',[] )
+            let userWishlist = []
+            userWishlist.push( item.id )
+            localStorage.setItem( 'wishlist', JSON.stringify( userWishlist ) )
+            console.log(JSON.parse(localStorage.getItem('wishlist')))
+        }
+    }
 
     const headers = {
         'Content-Type': 'application/json',
@@ -41,7 +65,7 @@ function Card ({item})
             <div class="card border-0 ">
 
                 {/* Heart Icon */ }
-                <button className="btn position-absolute translate-middle badge rounded-pill bg-white heart_icon_container " style={ { top: 30, right: -5 } }><svg width="22" height="18" viewBox="0 0 24 21" fill="none" className='heart_icon ' xmlns="http://www.w3.org/2000/svg">
+                <button onClick={ handleWishlist } className="btn position-absolute translate-middle badge rounded-pill bg-white heart_icon_container " style={ { top: 30, right: -5 } }><svg width="22" height="18" viewBox="0 0 24 21" fill="none" className='heart_icon ' xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /></svg>
                 </button>
 
