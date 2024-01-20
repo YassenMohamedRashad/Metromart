073f581e5e5929424ff7fa26c550ad75636c3df5
AhmadOthman <trollingosman101@outlook.com>
2024-01-20 23:14:20 +0200

    .

--- src/Components/utils/Card.jsx
+++ src/Components/utils/Card.jsx
@@ -94,7 +94,7 @@ function Card({ item }) {
 				</button>
 				<div className="d-flex justify-content-center product-img">
 					<img
-						src={JSON.parse(item.image_path)[0]}
+						src={item.images[0]}
 						className=" border-0 rounded-2 shadow"
 						alt="Title"
 					/>
