8dbb27b1e8822a7b1958029853d90818fb16a80f
AhmadOthman <trollingosman101@outlook.com>
2024-01-20 03:27:37 +0200

    -  feat(Error.jsx): Update Error component to use react-router-dom's Link component instead of anchor tag for navigation -  style(Wishlist.css): Refactor CSS for trash-icon and eye-icon in Wishlist component for better readability and maintainability

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,31 +1,30 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/css/Error.css";
-import { useNavigate } from "react-router-dom";
 
-function Error ()
-{
-    const ErrorStyle = {
-        height: "80vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    }
-    const heading = {
-        fontSize:"70px"
-    }
-    return (
-        <div className="container" style={ErrorStyle}>
-                <div className="text-center">
-                <h1 className="" style={ heading }>404 Not Found</h1>
-                    <p className="">
-                        your visited page not found, you may go home page.
-                    </p>
-                    <a href="/Metromart/" className="btn-danger btn">
-                        Back to home page
-                    </a>
-                </div>
-        </div>
-    );
+function Error() {
+	const ErrorStyle = {
+		height: "80vh",
+		display: "grid",
+		"place-items": "center",
+	};
+	const heading = {
+		fontSize: "4.375rem",
+	};
+	return (
+		<div className="container" style={ErrorStyle}>
+			<div className="text-center">
+				<h1 className="" style={heading}>
+					404 Page Not Found
+				</h1>
+				<p className="text-capitalize">
+					your visited page not found, you may go to the home page.
+				</p>
+				<Link to="/Metromart/" className="btn-danger btn">
+					Back to home page
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Error;
