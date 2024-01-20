69b162cd927df66752722f93a3700d16fab9d02f
AhmadOthman <trollingosman101@outlook.com>
2024-01-20 03:52:45 +0200

    .

--- src/Components/utils/Card.jsx
+++ src/Components/utils/Card.jsx
@@ -5,7 +5,7 @@ import useWishlist from "../../Hooks/useWishlist";
 import { StarRating } from "../productDetailsComponents/StarRating";
 
 function Card({ item }) {
-	const { user, user_token, wishlist, dispatch } = useAuth();
+	const { user, user_token } = useAuth();
     const { inWishlist, handleWishlistToggle } = useWishlist(item.id);
 	const headers = {
 		"Content-Type": "application/json",
@@ -95,7 +95,7 @@ function Card({ item }) {
 				</button>
 				<div className="d-flex justify-content-center product-img">
 					<img
-						src={item.images[0]}
+						src={item.image_path[0]}
 						className=" border-0 rounded-2 shadow"
 						alt="Title"
 					/>
